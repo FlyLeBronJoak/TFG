be797fba6511c642bc6666b12cbd1302d09c490d
Administrator <Administrator@pc.localdomain>
2018-11-15 18:30:21 +0100

    COMIENZO

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>GAME CHALLENGE</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+    </head>
+    <body class="landing is-preload">
+
+        <!-- Page Wrapper -->
+        <div id="page-wrapper">
+
+            <!-- Header -->
+            <header id="header" class="alt">
+                <h1><a href="index.html">Spectral</a></h1>
+                <nav id="nav">
+                    <ul>
+                        <li class="special">
+                            <a href="#menu" class="menuToggle"><span>Menu</span></a>
+                            <div id="menu">
+                                <ul>
+                                    <li><a href="index.php">Inicio</a></li>
+                                    <li><a href="fechasTorneos.php">Fechas de Torneos</a></li>
+                                    <li><a href="elements.html">Noticias</a></li>
+                                    
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+
+            <!-- Banner -->
+            <section id="banner">
+                <div class="inner">
+                    <h2>GAME CHALLENGE</h2>
+                    <p>Juega con tus amigos<br />
+                        y gana increibles recompensas<br />
+                        para canjear en <a href="https://store.steampowered.com/?l=spanish">STEAM</a></p>
+                    <ul class="actions special">
+                        <li><a href="#" class="button primary">INCIAR SESION</a></li>
+                        <li><a href="#" class="button primary">REGISTRARSE</a></li>
+                    </ul>
+                </div>
+                <a href="#footer" class="more scrolly">REDES SOCIALES</a>
+            </section>
+
+            <!-- One -->
+            
+
+            <!-- Footer -->
+            <footer id="footer">
+                <ul class="icons">
+                    <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                    <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                    <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                    <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+                </ul>
+                <ul class="copyright">
+                    <li>&copy; GAME CHALLENGE</li><li>Creada por: <a href="http://html5up.net">Joaquin Sierra</a></li>
+                </ul>
+            </footer>
+
+        </div>
+
+        <!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/jquery.scrollex.min.js"></script>
+        <script src="assets/js/jquery.scrolly.min.js"></script>
+        <script src="assets/js/browser.min.js"></script>
+        <script src="assets/js/breakpoints.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
